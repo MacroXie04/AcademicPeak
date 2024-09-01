ce973b5296bbc56732dc01b5db87c4c7606991b1
MacroXie <xiehongzhe04@gmail.com>
2024-08-31 21:56:03 -0700

    updated html pages for ranking

--- ranking/views.py
+++ ranking/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 def ranking_index(request):
     return render(request, 'index_ranking.html')
 
+def ranking_login(request):
+    return render(request, 'user_page/login_ranking.html')
+
+
+def ranking_search(request):
+    return render(request, 'ranking_page/search_ranking.html')
+
+def ranking_ranking(request):
+    return render(request, 'ranking_page/ranking_ranking.html')
+
+
